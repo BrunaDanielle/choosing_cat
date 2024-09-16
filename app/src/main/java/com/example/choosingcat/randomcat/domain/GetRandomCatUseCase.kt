0594258bd35529e66786474bc9bfedac788870b7
Bruna <brunadanielle3@hotmail.com>
2024-09-16 20:25:09 -0300

    feat: add random cat usecase

--- app/src/main/java/com/example/choosingcat/randomcat/domain/GetRandomCatUseCase.kt
+++ app/src/main/java/com/example/choosingcat/randomcat/domain/GetRandomCatUseCase.kt
@@ -3,6 +3,6 @@ package com.example.choosingcat.randomcat.domain
 import com.example.choosingcat.randomcat.domain.model.Cat
 import kotlinx.coroutines.flow.Flow
 
-class GeRandomCatUseCase(private val repository: RandomCatRepository) {
+class GetRandomCatUseCase(private val repository: RandomCatRepository) {
     operator fun invoke(): Flow<Cat> = repository.getRandomCat()
 }
